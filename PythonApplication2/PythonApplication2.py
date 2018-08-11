9d8325f5e6967ae33e7f96d558482a85c9ed4d27
Janghoon Lee <chaossky@naver.com>
2018-08-11 20:22:34 +0900

    barchart

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -1,1 +1,13 @@
+import matplotlib.pyplot as plt
 
+x1=[1,2,3,4,5,6,7]
+y1=[4,5,6,6,8,7,9]
+
+plt.bar(x1,y1,label='Bars 1')
+
+
+plt.xlabel('x')   
+plt.ylabel('y')
+plt.title('Kora\n Check it out' )
+plt.legend()
+plt.show()
